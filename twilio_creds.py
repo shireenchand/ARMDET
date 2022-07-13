317f17ed8900c98fd7e02658b1c815ce3f974dec
Shireen Chand <72601697+shireenchand@users.noreply.github.com>
2022-07-13 11:37:36 +0530

    Update twilio_creds.py

--- twilio_creds.py
+++ twilio_creds.py
@@ -1,3 +1,3 @@
-auth_sid = "AC7d8b031e7c3ce06fb86507b8a7de7db5"
-auth_token = "d47678dbf9a78daf6e99b99305d51d99"
-phone_no = '+14342160599'
+auth_sid = "AUTH SID"
+auth_token = "AUTH TOKEN"
+phone_no = 'PHONE NUMBER'
